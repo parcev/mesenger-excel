3f722554b9c7aa0070faaa68d8715d1b5bc798be
majnkraaft <169532258+majnkraaft@users.noreply.github.com>
2026-09-16 16:42:45 +0300

    update index.js

--- index.js
+++ index.js
@@ -369,5 +369,9 @@ app.post("/webhook", async (req, res) => {
   }
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => logStep("SERVER", `Server listening on port ${PORT}`));
